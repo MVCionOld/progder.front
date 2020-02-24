60feba960b3fc34d96f9283fe2d213ce927590a7
tsion mihail <tsion.m@phystech.edu>
2020-02-25 01:47:10 +0300

    Developed EngagementItem for CandidateWorkspace

--- src/components/EngagementItem.js
+++ src/components/EngagementItem.js
@@ -1,7 +1,6 @@
 import React, {Component} from "react";
 import './EngagementItem.css';
 import {EngagementButton} from "./EngagementButton";
-import {SubmitButton} from "./SubmitButton";
 
 
 export class EngagementItem extends Component {
@@ -9,7 +8,8 @@ export class EngagementItem extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            mounted: true
+            mounted: true,
+            accepted: null
         };
         this.handleUnmounting = this.handleUnmounting.bind(this);
         this.handleUnmountWithAcceptance = this.handleUnmountWithAcceptance.bind(this);
@@ -24,41 +24,58 @@ export class EngagementItem extends Component {
 
     handleUnmountWithAcceptance() {
         console.log("Accepted");
-        setTimeout(this.handleUnmounting, 1100);
+        this.setState((props, state) => ({
+            accepted: true
+        }));
+        setTimeout(this.handleUnmounting, 1200);
     }
 
     handleUnmountWithRecejtion() {
         console.log("Rejected");
-        setTimeout(this.handleUnmounting, 1100);
+        this.setState((props, state) => ({
+            accepted: false
+        }));
+        setTimeout(this.handleUnmounting, 1200);
     }
 
     render = () => {
+        const flipCardAcceptClass = this.state.accepted === true
+            ? "accept activated"
+            : "accept";
+        const flipCardRejectClass = this.state.accepted === false
+            ? "reject activated"
+            : "reject";
+        const contentClass = this.state.accepted === null
+            ? "content"
+            : (this.state.accepted
+                ? "content selected accepted"
+                : "content selected rejected");
         return (
             this.state.mounted &&
-            <div className="engagement-item">
-                <div className="content">
-                    <div>
-                        <h3>Invite from {this.props.recruiterLogin}</h3>
-                        <p>Company: {this.props.companyName}</p>
-                        <div className="btns">
-                            <EngagementButton
-                                type={"accept-btn"}
-                                onClick={this.handleUnmountWithAcceptance}>
-                                Accept
-                            </EngagementButton>
-                            <EngagementButton
-                                type={"reject-btn"}
-                                onClick={this.handleUnmountWithRecejtion}>
-                                Reject
-                            </EngagementButton>
-                        </div>
+            <div className={"engagement-item"}>
+                <div className={"flip-card"}>
+                    <div className={flipCardAcceptClass}>
+                        <h2>accept</h2>
                     </div>
-                    {/*<div className={"face accept"}>*/}
-                    {/*    <h2>accept</h2>*/}
-                    {/*</div>*/}
-                    {/*<div className={"face reject"}>*/}
-                    {/*    <h2>reject</h2>*/}
-                    {/*</div>*/}
+                    <div className={flipCardRejectClass}>
+                        <h2>reject</h2>
+                    </div>
+                </div>
+                <div className={contentClass}>
+                    <h3>Invite from<br/>{this.props.recruiterLogin}</h3>
+                    <p>Company:<br/>{this.props.companyName}</p>
+                    <div className="btns">
+                        <EngagementButton
+                            type={"accept-btn"}
+                            onClick={this.handleUnmountWithAcceptance}>
+                            Accept
+                        </EngagementButton>
+                        <EngagementButton
+                            type={"reject-btn"}
+                            onClick={this.handleUnmountWithRecejtion}>
+                            Reject
+                        </EngagementButton>
+                    </div>
                 </div>
             </div>
         );
