f9a7bebf1b7f14aa285fcf3aa51ad3625c1cbd87
tsion mihail <tsion.m@phystech.edu>
2020-02-25 03:28:53 +0300

    Created menus for role and login/sign up action

---
+++ src/components/SelectionMenu.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import "./SelectionMenu.css";
+
+
+const Selection = props => <li>
+    <a href={props.href} data-text={props.children.toUpperCase()}>{props.children}</a>
+</li>;
+
+export class EnterMenu extends Component {
+    render() {
+        const role = this.props.match.params.role;
+        return (
+            <div className="navigation">
+                <ul>
+                    <Selection href={`/${role}/login`}>Login</Selection>
+                    <Selection href={`/${role}/signup`}>Signup</Selection>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export class RoleMenu extends Component {
+    render() {
+        return (
+            <div className="navigation">
+                <ul>
+                    <Selection href={"/candidate"}>Candidate</Selection>
+                    <Selection href={"/recruiter"}>Recruiter</Selection>
+                </ul>
+            </div>
+        );
+    }
+}
+
