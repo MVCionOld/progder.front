cbade70a505358b4206a769f3c63f5444c10e76b
tsion mihail <tsion.m@phystech.edu>
2020-03-09 21:23:31 +0300

    Refactor

--- src/components/selectionmenu/SelectionMenu.js
+++ src/components/selectionmenu/SelectionMenu.js
@@ -2,13 +2,15 @@ import React, {Component} from 'react';
 import "./SelectionMenu.css";
 
 
-const Selection = props => <li>
-    <a
-        href={props.href}
-        data-text={props.children.toUpperCase()}>
-        {props.children}
-    </a>
-</li>;
+const Selection = props => (
+    <li>
+        <a
+            href={props.href}
+            data-text={props.children.toUpperCase()}>
+            {props.children}
+        </a>
+    </li>
+);
 
 export class EnterMenu extends Component {
     render() {
