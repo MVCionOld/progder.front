b750a9907722390e3562ae03152a9a0b0d148c83
tsion mihail <tsion.m@phystech.edu>
2020-03-12 02:31:25 +0300

    Added redux-storage

--- src/index.js
+++ src/index.js
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {BrowserRouter} from "react-router-dom";
 import MainRouter from "./routes";
+import {Provider} from "react-redux";
+import {storeFactory} from "./reducers";
 
+
+const initialState = {};
+
+export const store = storeFactory(initialState);
 
 const render = () => ReactDOM.render(
-    <BrowserRouter>
-        <MainRouter/>
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <MainRouter/>
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root')
 );
 
+store.subscribe(render);
 render();
